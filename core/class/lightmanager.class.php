c91fd82fc2f12998a7811ada8a751c904bb03b65
Loïc <loic@jeedom.com>
2019-11-22 12:58:24 +0100

    test

--- core/class/lightmanager.class.php
+++ core/class/lightmanager.class.php
@@ -26,20 +26,234 @@ class lightmanager extends eqLogic {
   /*     * ***********************Methode static*************************** */
   
   public static function mainLightChange($_options){
-    
+    $lightmanager = self::byId($_options['lightmanager_id']);
+    if(!is_object($lightmanager)){
+      return;
+    }
+    $stateHandling = $lightmanager->getCmd(null, 'stateHandling');
+    if($stateHandling->execCmd() == 0){
+      return;
+    }
+    $lightState = $lightmanager->getLightState();
+    if($lightState != $lightmanager->getCache('lastLightOrder',$lightState) && $lightmanager->getConfiguration('auto_walkout') == 1){
+      $stateHandling->event(0);
+      return;
+    }
+    if($lightState){
+      $crons = cron::searchClassAndFunction('lightmanager', 'autoLightOff', '"lightmanager_id":"' . $lightmanager->getId());
+      if (is_array($crons)) {
+        foreach ($crons as $cron) {
+          if ($cron->getState() != 'run') {
+            $cron->remove();
+          }
+        }
+      }
+      $cron = new cron();
+      $cron->setClass('lightmanager');
+      $cron->setFunction('autoLightOff');
+      $cron->setOption(array('lightmanager_id' => intval($lightmanager->getId())));
+      $cron->setLastRun(date('Y-m-d H:i:s'));
+      $cron->setOnce(1);
+      $cron->setSchedule(cron::convertDateToCron(strtotime('now') + 60 * $lightmanager->getConfiguration('delay::max_on')));
+      $cron->save();
+    }
   }
   
   public static function mainMotionChange($_options){
-    
+    $lightmanager = self::byId($_options['lightmanager_id']);
+    if(!is_object($lightmanager)){
+      return;
+    }
+    $lightmanager->handleStateLight();
+  }
+  
+  public static function autoMotionLightOff($_options){
+    $lightmanager = self::byId($_options['lightmanager_id']);
+    if(!is_object($lightmanager)){
+      return;
+    }
+    $stateHandling = $lightmanager->getCmd(null, 'stateHandling');
+    if($stateHandling->execCmd() == 0){
+      return;
+    }
+    $lightmanager->lightOff();
+  }
+  
+  public static function autoLightOff($_options){
+    $lightmanager = self::byId($_options['lightmanager_id']);
+    if(!is_object($lightmanager)){
+      return;
+    }
+    $stateHandling = $lightmanager->getCmd(null, 'stateHandling');
+    if($stateHandling->execCmd() == 0){
+      return;
+    }
+    $lightmanager->lightOff();
   }
   
   public static function mainLuminosityChange($_options){
-    
+    $lightmanager = self::byId($_options['lightmanager_id']);
+    if(!is_object($lightmanager)){
+      return;
+    }
+  }
+  
+  public static function mainHandleChange($_options){
+    $lightmanager = self::byId($_options['lightmanager_id']);
+    if(!is_object($lightmanager)){
+      return;
+    }
+    $crons = cron::searchClassAndFunction('lightmanager', 'autoLightOff', '"lightmanager_id":"' . $lightmanager->getId());
+    if (is_array($crons)) {
+      foreach ($crons as $cron) {
+        if ($cron->getState() != 'run') {
+          $cron->remove();
+        }
+      }
+    }
+    $stateHandling = $lightmanager->getCmd(null, 'stateHandling');
+    if($stateHandling->execCmd() == 0 && $lightmanager->getConfiguration('delay::regain_control') > 0){
+      $cron = new cron();
+      $cron->setClass('lightmanager');
+      $cron->setFunction('resumeHandling');
+      $cron->setOption(array('lightmanager_id' => intval($lightmanager->getId())));
+      $cron->setLastRun(date('Y-m-d H:i:s'));
+      $cron->setOnce(1);
+      $cron->setSchedule(cron::convertDateToCron(strtotime('now') + 60 * $lightmanager->getConfiguration('delay::regain_control')));
+      $cron->save();
+    }
+  }
+  
+  public function resumeHandling($_options){
+    $lightmanager = self::byId($_options['lightmanager_id']);
+    if(!is_object($lightmanager)){
+      return;
+    }
+    $lightmanager->handleStateLight();
   }
   
   
   /*     * *********************Méthodes d'instance************************* */
   
+  public function handleStateLight(){
+    $stateHandling = $this->getCmd(null, 'stateHandling');
+    if($stateHandling->execCmd() == 0){
+      return;
+    }
+    $lightState = $this->getLightState();
+    if($lightState != $this->getCache('lastLightOrder',$lightState) && $this->getConfiguration('auto_walkout') == 1){
+      $stateHandling->event(0);
+      return;
+    }
+    if($stateHandling->execCmd() == 0){
+      return;
+    }
+    $motionState = $this->getMotionState();
+    if($motionState == $this->getCache('lastMotionOrder',$motionState)){
+      return;
+    }
+    $this->setCache('lastMotionOrder',$motionState)
+    $crons = cron::searchClassAndFunction('lightmanager', 'autoMotionLightOff', '"lightmanager_id":"' . $this->getId());
+    if (is_array($crons)) {
+      foreach ($crons as $cron) {
+        if ($cron->getState() != 'run') {
+          $cron->remove();
+        }
+      }
+    }
+    $luminosityState = $this->getLuminosityState();
+    if($motionState){
+      if(!$luminosityState){
+        return;
+      }
+      $this->lightOn();
+    }else{
+      if($this->getConfiguration('delay::off_no_motion') <= 0){
+        $this->lightOff();
+      }else{
+        $cron = new cron();
+        $cron->setClass('lightmanager');
+        $cron->setFunction('autoMotionLightOff');
+        $cron->setOption(array('lightmanager_id' => intval($this->getId())));
+        $cron->setLastRun(date('Y-m-d H:i:s'));
+        $cron->setOnce(1);
+        $cron->setSchedule(cron::convertDateToCron(strtotime('now') + 60 * $this->getConfiguration('delay::off_no_motion')));
+        $cron->save();
+      }
+    }
+  }
+  
+  public function lightOn(){
+    $lights = $this->getConfiguration('lights','');
+    if($lights != '' ){
+      foreach ($lights as $light) {
+        try {
+          $cmd = cmd::byId($light['cmdOn']);
+          if(!is_object($cmd)){
+            continue;
+          }
+          $cmd->execCmd($light['options']);
+        } catch (\Exception $e) {
+          
+        }
+      }
+    }
+    $lightmanager->setCache('lastLightOrder',1);
+  }
+  
+  public function lightOff(){
+    $lights = $this->getConfiguration('lights','');
+    if($lights != '' ){
+      foreach ($lights as $light) {
+        try {
+          $cmd = cmd::byId($light['cmdOff']);
+          if(!is_object($cmd)){
+            continue;
+          }
+          $cmd->execCmd();
+        } catch (\Exception $e) {
+          
+        }
+      }
+    }
+    $lightmanager->setCache('lastLightOrder',0);
+  }
+  
+  public function getMotionState(){
+    $motions = $this->getConfiguration('motions','');
+    if($motions != '' ){
+      foreach ($motions as $motion) {
+        if(cmd::cmdToValue($motion['cmdMotion']) == 1){
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  
+  public function getLuminosityState(){
+    $luminositys = $this->getConfiguration('luminositys','');
+    if($luminositys != '' ){
+      foreach ($luminositys as $luminosity) {
+        if(cmd::cmdToValue($luminosity['cmdLuminosity']) < $luminosity['threshold']){
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  
+  public function getLightState(){
+    $lights = $this->getConfiguration('lights','');
+    if($lights != '' ){
+      foreach ($lights as $light) {
+        if(cmd::cmdToValue($light['cmdState'])){
+          return true;
+        }
+      }
+    }
+    return false;
+  }
   
   public function postSave() {
     $cmd = $this->getCmd(null, 'stateHandling');
@@ -86,6 +300,29 @@ class lightmanager extends eqLogic {
     $cmd->setEqLogic_id($this->getId());
     $cmd->save();
     
+    $cmd = $this->getCmd(null, 'on');
+    if (!is_object($cmd)) {
+      $cmd = new lightmanagerCmd();
+      $cmd->setLogicalId('on');
+      $cmd->setName(__('On', __FILE__));
+    }
+    $cmd->setType('action');
+    $cmd->setSubType('other');
+    $cmd->setEqLogic_id($this->getId());
+    $cmd->save();
+    
+    
+    $cmd = $this->getCmd(null, 'off');
+    if (!is_object($cmd)) {
+      $cmd = new lightmanagerCmd();
+      $cmd->setLogicalId('off');
+      $cmd->setName(__('Off', __FILE__));
+    }
+    $cmd->setType('action');
+    $cmd->setSubType('other');
+    $cmd->setEqLogic_id($this->getId());
+    $cmd->save();
+    
     $lights = $this->getConfiguration('lights','');
     if($lights != '' ){
       $listener = listener::byClassAndFunction('lightmanager', 'mainLightChange', array('lightmanager_id' => intval($this->getId())));
@@ -98,7 +335,7 @@ class lightmanager extends eqLogic {
       $listener->emptyEvent();
       $nblistener = 0;
       foreach ($lights as $light) {
-        preg_match_all("/#([0-9]*)#/", $light['cmdOn'], $matches);
+        preg_match_all("/#([0-9]*)#/", $light['cmdState'], $matches);
         foreach ($matches[1] as $cmd_id) {
           $nblistener += 1;
           $listener->addEvent($cmd_id);
@@ -184,7 +421,16 @@ class lightmanagerCmd extends cmd {
   /*     * *********************Methode d'instance************************* */
   
   public function execute($_options = array()) {
-    
+    $lightmanager = $this->getEqLogic();
+    if($this->getLogicalId() == 'on'){
+      $lightmanager->lightOn();
+    }else if($this->getLogicalId() == 'off'){
+      $lightmanager->lightOff();
+    }else if($this->getLogicalId() == 'resumeHandling'){
+      $lightmanager->getCmd(null, 'stateHandling')->event(1);
+    }else if($this->getLogicalId() == 'suspendHandling'){
+      $lightmanager->getCmd(null, 'stateHandling')->event(0);
+    }
   }
   
   /*     * **********************Getteur Setteur*************************** */
