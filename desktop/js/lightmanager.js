8222cfcc0b8896215491376ee1f2aaa6d575fa58
Loïc <loic@jeedom.com>
2022-11-14 11:45:40 +0100

    fix function

--- desktop/js/lightmanager.js
+++ desktop/js/lightmanager.js
@@ -96,7 +96,7 @@ $("#div_mainContainer").off('click','.listCmdAction').on('click','.listCmdAction
     if(el.closest('.form-group').find('.actionOptions').html() != undefined){
       jeedom.cmd.displayActionOption(result.human, '', function (html) {
         el.closest('.form-group').find('.actionOptions').html(html);
-        taAutosize();
+        jeedomUtils.taAutosize();
       });
     }
   });
@@ -107,7 +107,7 @@ $('body').off('focusout','.expressionAttr[data-l1key=cmdOn]').on('focusout','.ex
   var expression = $(this).closest('.form-group').getValues('.expressionAttr');
   jeedom.cmd.displayActionOption($(this).value(), init(expression[0].options), function (html) {
     el.closest('.form-group').find('.actionOptions').html(html);
-    taAutosize();
+    jeedomUtils.taAutosize();
   })
 });
 
@@ -133,7 +133,7 @@ function addLight(_light) {
   div += '</span>';
   div += '</div>';
   div += '</div>';
-  var actionOption_id = uniqId();
+  var actionOption_id = jeedomUtils.uniqId();
   div += '<div class="col-sm-5 actionOptions" id="'+actionOption_id+'"></div>';
   div += '</div>';
   div += '<div class="form-group">';
@@ -259,7 +259,7 @@ function printEqLogic(_eqLogic) {
       for(var i in data){
         $('#'+data[i].id).append(data[i].html.html);
       }
-      taAutosize();
+      jeedomUtils.taAutosize();
     }
   });
 }
